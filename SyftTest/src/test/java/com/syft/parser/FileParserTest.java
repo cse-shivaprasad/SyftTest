da25b9737c40c4eab86c60376012d6a1ad56d04e
Shivaprasad <cse.shivaprasad@gmail.com>
2021-05-23 18:12:17 +0100

    Moved the test files from main to test directories

--- SyftTest/src/test/java/com/syft/parser/FileParserTest.java
+++ SyftTest/src/test/java/com/syft/parser/FileParserTest.java
@@ -10,20 +10,20 @@ public class FileParserTest {
 	
 	@Test
 	public void should_return_record_count_excluding_header() throws IOException{
-		String[] inputRecords = FileParser.parseInputFile("src/main/resources/test-input/input1.csv", true);
+		String[] inputRecords = FileParser.parseInputFile("src/test/resources/test-input/input1.csv", true);
 		Assert.assertEquals(7, inputRecords.length);
 	}
 	
 	@Test
 	public void should_return_record_all_record_count() throws IOException{
-		String[] inputRecords = FileParser.parseInputFile("src/main/resources/test-input/input1.csv", false);
+		String[] inputRecords = FileParser.parseInputFile("src/test/resources/test-input/input1.csv", false);
 		Assert.assertEquals(8, inputRecords.length);
 	}
 	
 	@Test
 	public void should_throw_IOException() {
 		IOException exception = Assert.assertThrows(IOException.class,
-									() -> FileParser.parseInputFile("main/resources/test-inputs/input3.csv", true));
+									() -> FileParser.parseInputFile("test/resources/test-inputs/input3.csv", true));
 		Assert.assertTrue(exception != null);
 	} 
 
