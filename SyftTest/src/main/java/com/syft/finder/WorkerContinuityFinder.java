8350b91ffd5b302e0f3ba5fe2a50c373f55993f0
Shivaprasad <cse.shivaprasad@gmail.com>
2021-06-14 08:26:25 +0100

    Modified the tests and logic

--- SyftTest/src/main/java/com/syft/finder/WorkerContinuityFinder.java
+++ SyftTest/src/main/java/com/syft/finder/WorkerContinuityFinder.java
@@ -6,6 +6,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -13,44 +14,46 @@ public class WorkerContinuityFinder {
 	
 	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss[.SSS]");
 	
-	public Map<String,Integer> generateWorkerContinuityReport(String[] input, boolean isSortedByDate) throws IllegalArgumentException{
+	public Map<String,Integer> generateWorkerContinuityReport(String[] inputRows, boolean isSortedByDate) throws IllegalArgumentException{
 		
-		if(input == null || input.length == 0)
+		if(inputRows == null || inputRows.length == 0)
 			throw new IllegalArgumentException("Input array must be contain elements");
 		
 		if(!isSortedByDate)
-			Arrays.sort(input,(x,y)->compareDates(x, y));
+			Arrays.sort(inputRows,(x,y)->compareDates(x, y));
 		
 		Map<String, Integer> output = new HashMap<String, Integer>();
-		Map<String,List<LocalDateTime>> workerGroupingMap = getWorkerGroupingMap(input,formatter);
+		Map<String,List<LocalDateTime>> workerGroupingMap = getWorkerGroupingMap(inputRows,formatter);
+		int workerGroupKeyCounter = 0;
 		for(String key : workerGroupingMap.keySet()) {
 			String workerId = key.split("_")[0];
 			int counter = 0;
 			List<LocalDateTime> values = workerGroupingMap.get(key);
 			for(int i=0; i < values.size(); i++) {
 				if(i == 0 || Math.abs(values.get(i).until(values.get(i-1), ChronoUnit.DAYS)) <=6) {
-					counter ++;
+					counter++;
 				}
 				else {
-					output.put(workerId, Math.max(counter,output.getOrDefault(workerId, 0)));
-					counter = 0;
+					output.put(workerId, Math.max(counter,output.getOrDefault(workerId, 1)));
+					counter = 1;
 				}	
 			}
-			output.put(workerId, Math.max(counter,output.getOrDefault(workerId, 0)));
-			counter = 0;
+			workerGroupKeyCounter++;
+			if(workerGroupKeyCounter < workerGroupingMap.keySet().size())
+				counter = 0;	
 		}
 		return output;
 	}
 	
-	
-	private Map<String,List<LocalDateTime>> getWorkerGroupingMap(String[] input, DateTimeFormatter dateTimeFormater){
+	private Map<String,List<LocalDateTime>> getWorkerGroupingMap(String[] inputRows, DateTimeFormatter dateTimeFormater){
 		Map<String, List<LocalDateTime>> workerGroupingMap = new HashMap();
-		for(int i=0; i<input.length; i++) {
-			String[] values = input[i].split(",");
+		
+		for(String row : inputRows) {
+			String[] values = row.split(",");
 			String key = values[0].trim()+"_"+values[1].trim()+"_"+values[2].trim();
 			LocalDateTime date = LocalDateTime.parse(values[3].trim(),dateTimeFormater);
 			if(!workerGroupingMap.containsKey(key))
-				workerGroupingMap.put(key, new ArrayList<LocalDateTime>());
+				workerGroupingMap.put(key, new LinkedList<LocalDateTime>());
 			workerGroupingMap.get(key).add(date);
 		}
 		return workerGroupingMap;
@@ -59,6 +62,6 @@ public class WorkerContinuityFinder {
 	private int compareDates(String x, String y) {
 		LocalDateTime xDate = LocalDateTime.parse(x.split(",")[3].trim(),formatter);
 		LocalDateTime yDate = LocalDateTime.parse(y.split(",")[3].trim(),formatter);
-		return xDate.compareTo(yDate);
+		return yDate.compareTo(xDate);
 	}
 }
